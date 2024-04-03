36bb09094f586138eebb7832960bc0fc04a0e7fb
DenNeroA <dennis.rosales@uamcr.net>
2024-04-03 01:04:50 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Barralateral from './components/Barralateral';
+
+function App() {
+  return (
+    <div className="App">
+      <Barralateral/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Proyecto Final P4.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
+/////
